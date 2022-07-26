1f70335b70ab0f508c5142b0091b9280d860fa24
nikskul <nikitaskul2012@gmail.com>
2022-07-26 12:37:05 +0300

    Init

---
+++ src/main/java/com/nikskul/kafkabanking/KafkaBankingApplication.java
@@ -0,0 +1,13 @@
+package com.nikskul.kafkabanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikskul/kafkabanking/KafkaBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikskul.kafkabanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
